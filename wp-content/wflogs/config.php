6ef599abdc537dac570809885c7121f27c8ca40c
trukhina <trukhina@qarea.com>
2020-03-07 16:40:39 +0200

    Add/update plugins; add child theme

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1583407817;s:7:"authKey";s:64:"25<ygW& |p.NDZw>$;hQ&>w]c8e,b5sx%KfzOz;t1@Z]AKR=I*gDz7XF.)>Y=g.;";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2521;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"25<ygW& |p.NDZw>$;hQ&>w]c8e,b5sx%KfzOz;t1@Z]AKR=I*gDz7XF.)>Y=g.;";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2521;}
